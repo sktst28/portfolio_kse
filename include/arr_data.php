a12616f1cfb26d9e7e81e1e544e3c8c4ecfad2d7
sktst28 <sktst28@users.noreply.github.com>
2023-05-19 16:33:23 +0900

    init

---
+++ include/arr_data.php
@@ -0,0 +1,70 @@
+<?
+
+$site_title = ""; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "Template";
+$nav_1_1 = "Template";
+$nav_1_2 = "Board List";
+$nav_1_3 = "Calendar";
+$nav_1_4 = "Location";
+$nav_1_5 = "Find Account";
+$nav_1_6 = "SNS Login";
+$nav_1_7 = "Hover Effects";
+// $nav_1_2 = "dp3-test";
+// $nav_1_2_1 = "Template";
+// $nav_1_2_2 = "dp3-test-bravo";
+// $nav_1_2_3 = "dp3-test-charlie";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../template/template.html";
+$nav_1_1_href = "../template/template.html";
+$nav_1_2_href = "../template/board-list.html";
+$nav_1_3_href = "../template/calendar.html";
+$nav_1_4_href = "../template/location.html";
+$nav_1_5_href = "../template/find_account.html";
+$nav_1_6_href = "../template/sns-login.html";
+$nav_1_7_href = "../template/hover-effect.html";
+// $nav_1_2_href = "../test/dp3-test__alpha.html";
+// $nav_1_2_1_href = "../test/dp3-test__alpha.html";
+// $nav_1_2_2_href = "../test/dp3-test__bravo.html";
+// $nav_1_2_3_href = "../test/dp3-test__charlie.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
